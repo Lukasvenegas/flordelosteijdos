76736fb7c152a0d1dd55b60ad4e1a251ca72bb3e
Lukasvenegas <153865628+Lukasvenegas@users.noreply.github.com>
2025-06-14 12:14:15 -0400

    nuevos cambios realizados

--- script.js
+++ script.js
@@ -22,3 +22,22 @@ document.querySelector('.search-button').addEventListener('click', function() {
         searchInput.focus(); // Enfocar el campo de búsqueda al abrir
     }
 });
+
+// Función para filtrar los servicios según el texto ingresado en la barra de búsqueda
+document.getElementById('search').addEventListener('input', function () {
+    const query = this.value.toLowerCase(); // Obtener el texto ingresado
+    const servicios = document.querySelectorAll('.servicios'); // Obtener todos los servicios
+    
+    // Iterar sobre todos los servicios y filtrar en base a la búsqueda
+    servicios.forEach(function(servicio) {
+        const nombre = servicio.querySelector('h2').textContent.toLowerCase(); // Nombre del servicio
+        const descripcion = servicio.querySelector('p').textContent.toLowerCase(); // Descripción del servicio
+        
+        // Si el nombre o la descripción del servicio contiene el texto de búsqueda, mostrarlo
+        if (nombre.includes(query) || descripcion.includes(query)) {
+            servicio.style.display = ''; // Mostrar el servicio
+        } else {
+            servicio.style.display = 'none'; // Ocultar el servicio
+        }
+    });
+});
